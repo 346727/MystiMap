6d9ff0d90e48d891a6cd5b9ff23411ac06cc956e
346727 <346727@uwr.edu.pl>
2023-11-18 00:16:16 +0100

    Initial commit

---
+++ DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.Extensions.Configuration;
+
+namespace MystiMapWeb
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // connect to MySQL with connection string from app settings
+            var connectionString = _configuration.GetConnectionString("WebApiDatabase");
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        //public DbSet<User> Users { get; set; }
+    }
+}
